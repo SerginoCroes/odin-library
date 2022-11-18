e8073762f7243faa761ffc14c676db4ba6a82605
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-11-18 15:20:44 +0100

    boilerplate

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
+
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? 'read.' : 'not read yet.'}`;
+  }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
