774a143f3af354e1f41548ada8799d37211fde58
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-11-21 13:50:54 +0100

    basic input form finished

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const addButton = document.querySelector('#addbook');
+const formInput = document.querySelector('form');
+
+addButton.addEventListener('click', () => formInput.style.display = 'grid');
+
+formInput.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  console.log(e);
+
+  e.target.style.display = 'none';
+
+//  for (let item of e.target){
+//    console.log(item.value);
+//  }
+});
+
 let myLibrary = [];
+
 
 function Book(title, author, pages, hasRead) {
   this.title = title;
@@ -11,6 +29,12 @@ function Book(title, author, pages, hasRead) {
   }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, pages, hasRead) {  
+  myLibrary.push(new Book(title, author, pages, hasRead));
+}
+
+function loopThroughArray() {
+  for (let book in myLibrary){
+    console.log(myLibrary[book].info());
+  }
 }
