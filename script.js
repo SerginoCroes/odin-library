59013e8d579fe0b24d7fff525b4958c883a5bb61
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-11-22 15:01:40 +0100

    previous commit incomplete

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const addButton = document.querySelector('#addbook');
+const cancelButton = document.querySelector('#cancel');
 const container = document.querySelector('#container');
 const formInput = document.querySelector('form');
 
 addButton.addEventListener('click', () => formInput.style.display = 'grid');
+
+cancelButton.addEventListener('click', () => formInput.style.display = 'none');
 
 formInput.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -52,18 +55,29 @@ function loopThroughArray() {
 }
 
 function createDisplay(book) {
-  let div = document.createElement('div');
-  let author = document.createElement('p');
-  let title = document.createElement('p');
-  let pages = document.createElement('p');
+  const div = document.createElement('div');
+  const author = document.createElement('p');
+  const title = document.createElement('p');
+  const pages = document.createElement('p');
+  const hasRead = document.createElement('p');
+  const deleteButton = document.createElement('button');
 
   author.textContent = `Author: ${myLibrary[book].title}`;
   title.textContent = `Title: ${myLibrary[book].author}`;
   pages.textContent = `Pages: ${myLibrary[book].pages}`;
+  hasRead.textContent = myLibrary[book].hasRead ? 'read' : 'not read';
+  deleteButton.textContent = 'delete';
+
+  deleteButton.addEventListener('click', (e) => {
+    myLibrary.splice(e.target.parentNode.id, 1);
+    container.removeChild(e.target.parentNode);
+  });
 
   div.appendChild(author);
   div.appendChild(title);
   div.appendChild(pages);
+  div.appendChild(hasRead);
+  div.appendChild(deleteButton);
 
   div.classList.add('book');
   div.setAttribute('id', book);
